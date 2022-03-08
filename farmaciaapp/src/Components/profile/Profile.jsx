ef9789884ee5aaf68d9e666cc698d8680c6ef612
Joseph Moscoso <josephmoscoso70@hotmail.com>
2022-03-08 14:34:26 -0600

    cambios importantes pt2
    Aun falta pt3

--- farmaciaapp/src/Components/profile/Profile.jsx
+++ farmaciaapp/src/Components/profile/Profile.jsx
@@ -3,87 +3,110 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-bootstrap';
 import Button from "react-bootstrap/Button";
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
+import { Container } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import CardGroup from 'react-bootstrap/CardGroup';
 import { useNavigate } from "react-router-dom";
+import test2 from "../../assets/ImagenTest1.jpg";
 import './Profile.css';
 function Profile() {
+    const favs = [
+        { imagen: test2, nombre: "Pastilla para la gripe", Descripcion: "20 lps" },
+        { imagen: test2, nombre: "Pastilla para la gripe", Descripcion: "20 lps" },
+        { imagen: test2, nombre: "Pastilla para la gripe", Descripcion: "20 lps" },
+        { imagen: test2, nombre: "Pastilla para la gripe", Descripcion: "20 lps" }
+    ];
+
     const navigate = useNavigate();
     function handleClick(path) {
         navigate(path);
     }
     return (
-        <div className='contenedorUser'>
-            <div>
-                <Form className='perfil'>
-                    <Form.Group className="mb-3" controlId="nameProfile">
-                        <Form.Label>Nombre</Form.Label>
-                        <Form.Control type="text" placeholder="Jose Gomez" disabled />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="emailProfile">
-                        <Form.Label>Correo</Form.Label>
-                        <Form.Control type="email" placeholder="JoseGomez@gmail.com" disabled />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="phoneProfile">
-                        <Form.Label>Telefono</Form.Label>
-                        <Form.Control type="text" placeholder="0000-0000" disabled />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="addressProfile">
-                        <Form.Label>Direccion</Form.Label>
-                        <Form.Control type="text" placeholder="Res. Las Uvas, Tegucigalpa, Francisco Morazan" disabled />
-                    </Form.Group>
-                    <Button variant="info" type="submit">
-                        Editar perfil
-                    </Button>
-                </Form>
-            </div>
-            <div>
-                <h6>Medicamentos favoritos</h6>
-                <Row xs={1} md={2} className="g-4">
-
-                    <Card>
-                        <Card.Img variant="top" src="https://i0.wp.com/sunwahpanama.com/wp-content/uploads/2020/05/PANADOL-ULTRA.jpg?resize=256%2C256&ssl=1" />
-                        <Card.Body>
-                            <Card.Title>Panadol</Card.Title>
-                            <Card.Text>
-                                Panadol de 300 gr.
-                            </Card.Text>
-                            <Button variant="primary">Buscar</Button> <Button variant="danger">Eliminar de favoritos</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="https://images.rappi.com/products/f0d353d4-1777-4dd3-a6a7-f2bb558d2228.png?d=200x200&e=webp" />
-                        <Card.Body>
-                            <Card.Title>Aspirina</Card.Title>
-                            <Card.Text>
-                                Aspirina para adultos de 500 gr
-                            </Card.Text>
-                            <Button variant="primary">Buscar</Button> <Button variant="danger">Eliminar de favoritos</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Sudagrip</Card.Title>
-                            <Card.Text>
-                                Sudagrip de 50 gr
-                            </Card.Text>
-                            <Button variant="primary">Buscar</Button> <Button variant="danger">Eliminar de favoritos</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Glokemina</Card.Title>
-                            <Card.Text>
-                                Me lo invente
-                            </Card.Text>
-                            <Button variant="primary">Buscar</Button> <Button variant="danger">Eliminar de favoritos</Button>
-                        </Card.Body>
-                    </Card>
+        <Container fluid>
+            <Container fluid>
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        <Container fluid className="caja">
+                            <Form className='perfil'>
+                                <Form.Group className="mb-3" controlId="nameProfile">
+                                    <Form.Label>Nombre</Form.Label>
+                                    <Form.Control type="text" placeholder="Jose Gomez" disabled />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="emailProfile">
+                                    <Form.Label>Correo</Form.Label>
+                                    <Form.Control type="email" placeholder="JoseGomez@gmail.com" disabled />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="phoneProfile">
+                                    <Form.Label>Telefono</Form.Label>
+                                    <Form.Control type="text" placeholder="0000-0000" disabled />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="addressProfile">
+                                    <Form.Label>Direccion</Form.Label>
+                                    <Form.Control type="text" placeholder="Res. Las Uvas, Tegucigalpa, Francisco Morazan" disabled />
+                                </Form.Group>
+                                <Button variant="primary" type="submit"
+                                style={{
+                                    backgroundColor: "#5AC4FF",
+                                    borderColor: "#5AC4FF"
+                                }}
+                                >
+                                    Editar perfil
+                                </Button>
+                            </Form>
+                        </Container>
+                    </Col>
                 </Row>
-            </div>
-        </div>
+            </Container>
+            <Container fluid>
+                <Row>
+                    <h6>Medicamentos favoritos</h6>
+                    <Col className="d-flex justify-content-center">
+                        <Row xs={1} md={2} className="g-4">
+                            {Array.isArray(favs) && Boolean(favs.length) ? (
+                                favs.slice().map((elem, index) => {
+                                    return (
+                                        <Card className="tarjeta">
+                                            <div className="card-img-top">
+                                                <Card.Img className="w-100" variant="top" src={elem.imagen} />
+                                            </div>
+                                            <Card.Body>
+                                                <div className="d-flex justify-content-center">
+                                                    <Card.Title>{elem.nombre}</Card.Title>
+                                                </div>
+                                                <div className="d-flex justify-content-center">
+                                                    <Card.Text>
+                                                        {elem.Descripcion}
+                                                    </Card.Text>
+                                                </div>
+                                                <div className="d-flex justify-content-center">
+                                                    <Button variant="primary" type="submit"
+                                                    style={{
+                                                        width: "170px"
+                                                    }}>Buscar</Button>
+                                                </div>
+                                                <div className="d-flex justify-content-center">
+                                                    <Button variant="danger" type="submit"
+                                                    style={{
+                                                        marginTop: "2px"
+                                                    }}>Eliminar de favoritos</Button>
+                                                </div>
+                                            </Card.Body>
+                                        </Card>
+                                    );
+                                })
+                            ) : (
+                                <div className="d-flex justify-content-center">
+                                    <h1 className="text-center">
+                                        Lo siento, aun no hay favoritos :c
+                                    </h1>
+                                </div>
+                            )}
+                        </Row>
+                    </Col>
+                </Row>
+                <p></p>
+            </Container>
+        </Container>
     )
 }
 
