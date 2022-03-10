2a251ec29c6fc44a851a68b803ed09a0fff0e870
Joseph Moscoso <josephmoscoso70@hotmail.com>
2022-03-10 15:39:11 -0600

    Creacion de contexto de pagina

--- farmaciaapp/src/Components/Header/header.js
+++ farmaciaapp/src/Components/Header/header.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./header.css";
 
+import { auth } from "../../Firebase/firebase.utils";
 import { Button, Container } from "react-bootstrap";
 import { Col, Row } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
@@ -9,9 +10,18 @@ import { NavLink } from 'react-bootstrap';
 import searchIcon from "../../assets/search_black_24dp.svg";
 import { useNavigate } from "react-router-dom";
 import logito from "../../assets/logoProvisional.jpg";
+import { contextoUser } from "../../contexto/contexto";
+import carrito from "../../assets/compra.svg";
 
 function Header() {
     const navigate = useNavigate();
+    const user = useContext(contextoUser);
+
+    const handleSignOut = () => {
+        auth.signOut();
+        window.location.replace("/");
+    };
+
     function handleClick(path) {
         navigate(path);
     }
@@ -19,7 +29,7 @@ function Header() {
         <Container fluid>
             <Row>
                 <Col className="d-flex justify-content-start">
-                    <NavLink onClick={() => handleClick("/")}><img className="Logo" src={logito}/></NavLink>
+                    <NavLink onClick={() => handleClick("/")}><img className="Logo" src={logito} /></NavLink>
                 </Col>
                 <Col className="d-flex justify-content-center">
                     <Form.Control type="email" placeholder="Busqueda en catalogo"
@@ -40,20 +50,43 @@ function Header() {
                     </Row>
                 </Col>
                 <Col className="d-flex justify-content-end">
-                    <NavLink
-                        style={{
-                            marginTop: "0.4rem",
-                            color: "#000000"
-                        }}
-                    >Contactar</NavLink>
+                    {user.isAuth ? (
+                        <>
+                            <div>{`Hola ${user.email}`}</div>
+                            <NavLink
+                                style={{
+                                    marginTop: "0.4rem",
+                                    color: "#000000"
+                                }}
+                                onClick={() => handleClick("/Login")}
+                            ><img src={carrito} /> Lista</NavLink>
+                            
+                            <NavLink
+                                style={{
+                                    marginTop: "0.4rem",
+                                    color: "#000000"
+                                }}
+                                onClick={() => handleClick("/Login")}
+                            >Cerrar Sesión</NavLink>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink
+                                style={{
+                                    marginTop: "0.4rem",
+                                    color: "#000000"
+                                }}
+                            >Contactar</NavLink>
 
-                    <NavLink
-                        style={{
-                            marginTop: "0.4rem",
-                            color: "#000000"
-                        }}
-                        onClick={() => handleClick("/Login")}
-                    >Acceder</NavLink>
+                            <NavLink
+                                style={{
+                                    marginTop: "0.4rem",
+                                    color: "#000000"
+                                }}
+                                onClick={() => handleClick("/Login")}
+                            >Acceder</NavLink>
+                        </>
+                    )}
                 </Col>
             </Row>
         </Container>
